10d29addb92947e868eb517307c529db1ec662e0
wangguoshi <147616469@qq.com>
2019-05-17 22:08:13 +0800

    初步配置完成config

--- client/src/main/java/com/mfy/client/ClientController.java
+++ client/src/main/java/com/mfy/client/ClientController.java
@@ -2,11 +2,17 @@ package com.mfy.client;
 
 import com.mfy.feign.Feign4Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ClientController {
+
+    @Value("${eureka.instance.hostname}")
+    private String name;
 
     @Autowired
     private Feign4Client feign4Client;
@@ -21,5 +27,10 @@ public class ClientController {
     public String test1(){
         String find = feign4Client.find();
         return find;
+    }
+
+    @RequestMapping(value = "name")
+    public String name(){
+        return name;
     }
 }
